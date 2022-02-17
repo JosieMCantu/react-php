d4e39572076f017ab59cbc444dfff93735cf52fc
JosieMCantu <josiex2@gmail.com>
2022-02-16 20:37:28 -0800

    intial commit

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+import HomeContainer from '../containers/HomeContainer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <HomeContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/HomeContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Display from '../components/Display'
+
+function HomeContainer() {
+  return (
+    <div>
+      <h1>Home Container</h1>
+      <Display />
+    </div>
+
+  )
+}
+
+export default HomeContainer
